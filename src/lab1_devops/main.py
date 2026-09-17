e859e5657b4cd6fe71f0dddd3a8d297cafa307ba
anastasiiakhymych <anastasiia.khymych.kb.2025@lpnu.ua>
2026-09-17 20:02:31 +0300

    Add main entry point

---
+++ src/lab1_devops/main.py
@@ -0,0 +1,13 @@
+from lib import add_numbers, multiply_numbers
+
+
+def main():
+    a = 5
+    b = 13
+
+    print("Сума:", add_numbers(a, b))
+    print("Добуток:", multiply_numbers(a, b))
+
+
+if __name__ == "__main__":
+    main()
